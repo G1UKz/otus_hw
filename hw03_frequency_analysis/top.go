7566110b09a8e743d2459d2a3839032529424997
idkise10 <idkise10@mts.ru>
2023-09-09 18:50:25 +0300

    Optimization

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -14,14 +14,14 @@ func Top10(inputString string) []string {
 	splitedString := strings.Split(filteredString, " ")
 	filteredSlice := FilterString(splitedString)
 	outputMap := countStrings(filteredSlice)
-	output := formTop10(outputMap)
-	for i := 0; i < len(output); i++ {
+	output := sortTop10(outputMap)
+	if len(output) < 11 {
+		return []string{"Error, less than 10 words in input text"}
+	}
+	for i := 0; i < 11; i++ {
 		if output[i] == "-" {
 			output = append(output[:i], output[i+1:]...)
 		}
-	}
-	if len(output) < 11 {
-		return []string{"Error, less than 10 words in input text"}
 	}
 	return output[0:10]
 }
@@ -50,34 +50,16 @@ func countStrings(inputSlice []string) map[string]int {
 	return countedWords
 }
 
-func formTop10(inputMap map[string]int) []string {
-	top10 := make([]string, 0)
-	top10Sorted := make([]string, 0)
-	top10Unsorted := make(map[int][]string)
-	maxKey := 0
+func sortTop10(inputMap map[string]int) []string {
 	words := make([]string, 0)
 	for key := range inputMap {
 		words = append(words, key)
 	}
 	sort.SliceStable(words, func(i, j int) bool {
+		if inputMap[words[i]] == inputMap[words[j]] {
+			return words[i] < words[j]
+		}
 		return inputMap[words[i]] > inputMap[words[j]]
 	})
-	top10 = append(top10, words...)
-	for _, v := range top10 {
-		top10Unsorted[inputMap[v]] = append(top10Unsorted[inputMap[v]], v)
-	}
-
-	for appearance := range top10Unsorted {
-		if appearance > maxKey {
-			maxKey = appearance
-		}
-	}
-	for inc := maxKey; inc > 0; inc-- {
-		sort.SliceStable(top10Unsorted[inc], func(i, j int) bool {
-			return top10Unsorted[inc][i] < top10Unsorted[inc][j]
-		})
-		top10Sorted = append(top10Sorted, top10Unsorted[inc]...)
-	}
-
-	return top10Sorted
+	return words
 }
