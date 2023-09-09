8689826d3fea998f82fa9d1e84fa049352fca1ac
idkise10 <idkise10@mts.ru>
2023-09-09 12:21:32 +0300

    HW03 is completed

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -43,7 +43,18 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
-var textLatin = `They say that good books cannot become old. I can say the same about the books of a unique English writer of the 19th century, Charlotte Bronte. She is one of the best representatives of the English realism trend. My favourite book by Charlotte Bronte is the novel that brought her fame, «Jane Eyre». «Jane Eyre» is an autobiographical novel. The main character is a shy girl, who has, however, a strong will and is independent. From her childhood Jane learned that she could rely only on herself. She came from a poor family, studied at Lowood institution for poor children, where she had to face many difficulties. But she learned to overcome her fears and troubles. She was also able to sympathize with other people and give a helping hand in a difficult situation.
+var textLatin = `They say that good books cannot become old. I can
+say the same about the books of a unique English writer of the 19th
+century, Charlotte Bronte. She is one of the best representatives of
+the English realism trend. My favourite book by Charlotte Bronte is
+the novel that brought her fame, «Jane Eyre». «Jane Eyre» is an 
+autobiographical novel. The main character is a shy girl, who has,
+however, a strong will and is independent. From her childhood Jane 
+learned that she could rely only on herself. She came from a poor 
+family, studied at Lowood institution for poor children, where she
+had to face many difficulties. But she learned to overcome her fears 
+and troubles. She was also able to sympathize with other people and
+give a helping hand in a difficult situation.
 	`
 
 func TestTop10(t *testing.T) {
